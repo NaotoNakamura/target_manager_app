68f5feb7a047956f25eea1fd25c5130cf951cff2
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2021-03-04 21:10:35 +0900

    showusecaseの作成

---
+++ backend/packages/UseCase/TargetShowUseCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace packages\UseCase;
+
+class TargetShowUseCase
+{
+    public function __construct()
+    {
+
+    }
+
+    public function handle()
+    {
+
+    }
+}
---
+++ backend/packages/UseCase/TaskShowUseCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace packages\UseCase;
+
+class TaskShowUseCase
+{
+    public function __construct()
+    {
+
+    }
+
+    public function handle()
+    {
+
+    }
+}
