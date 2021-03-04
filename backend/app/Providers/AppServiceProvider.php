665ba5f06d7ae62810d787e3505dbaa12b18fa90
中村直人 <nakamura.naoto.infratop@gmail.com>
2021-03-04 19:47:30 +0900

    taregtのusecaseをサービスプロバイダに登録

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use packages\UseCase\TargetIndexUseCase;
+use packages\UseCase\TargetStoreUseCase;
+use packages\UseCase\TargetShowUseCase;
+use packages\UseCase\TargetUpdateUseCase;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +17,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('TargetIndexUseCase', TargetIndexUseCase::class);
+        $this->app->bind('TargetStoreUseCase', TargetStoreUseCase::class);
+        $this->app->bind('TargetShowUseCase', TargetShowUseCase::class);
+        $this->app->bind('TargetUpdateUseCase', TargetUpdateUseCase::class);
+        $this->app->bind('TargetDestroyUseCase', TargetDestroyUseCase::class);
     }
 
     /**
