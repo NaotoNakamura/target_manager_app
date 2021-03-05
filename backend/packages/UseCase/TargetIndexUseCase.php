1068a1977528eb388f71d47e59210a6839c812ee
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2021-03-05 19:28:07 +0900

    target一覧のusecase作成

--- backend/packages/UseCase/TargetIndexUseCase.php
+++ backend/packages/UseCase/TargetIndexUseCase.php
@@ -2,15 +2,28 @@
 
 namespace packages\UseCase;
 
+use packages\Domain\ITargetRepository;
+
 class TargetIndexUseCase
 {
-    public function __construct()
-    {
+    protected $targetRepository;
 
+    public function __construct(ITargetRepository $targetRepository)
+    {
+        $this->targetRepository = $targetRepository;
     }
 
     public function handle($currentUserId)
     {
-
+        $tests = $this->targetRepository->getAll($currentUserId);
+        $result = array_map(function ($s) {
+            return [
+                "id" => $s->id(),
+                "user_id" => $s->userId(),
+                "title" => $s->title(),
+                "tasks" => $s->tasks()
+            ];
+        }, $tests);
+        return $result;
     }
 }
