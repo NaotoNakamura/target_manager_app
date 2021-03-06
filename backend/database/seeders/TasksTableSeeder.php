b240e691a88e66a512e337a62665d85c9de2e62d
NaotoNakamura <nakamura.naoto.infratop@gmail.com>
2021-03-06 10:55:28 +0900

    usecaseのUT作成

--- backend/database/seeders/TasksTableSeeder.php
+++ backend/database/seeders/TasksTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TasksTableSeeder extends Seeder
 {
@@ -13,6 +14,21 @@ class TasksTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $tasks = [
+            [
+                'id' => 1,
+                'user_id' => 1,
+                'target_id' => 1,
+                'task_title' => 'タスクテスト1',
+                'period_kind' => 'DAY',
+                'start_date' => '2021/2/10',
+                'end_date' => '2021/2/10',
+                'is_done' => false,
+                'created_at' => '2021-02-13',
+                'updated_at' => '2021-02-13',
+            ],
+        ];
+ 
+        DB::table('tasks')->insert($tasks);
     }
 }
